1480ed7e30819d643b0f147eaa25e526dc14dd02
adityaMishra006 <mishraadityaa6@gmail.com>
2026-02-27 09:11:22 +0530

    printing welcome message

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp{
+    static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome checker management system");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
